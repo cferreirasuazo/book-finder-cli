03b04188aace7b16400678e3f98926f43da0be47
Cristhian Ferreira <cferreirasuazo@gmail.com>
2020-06-03 21:23:54 -0400

    Script for find books using Google Books API
    features:
     -click: Framework for create Python CLI Applications
     -requests: http library for python

---
+++ index.py
@@ -0,0 +1,45 @@
+from Book import Book
+import click
+import requests
+
+
+def getBooks(query):
+    # Function to get the books from the API
+    info = {}
+    URL = "https://www.googleapis.com/books/v1/volumes?q="
+    #with f-string we concatenates the URL with the query argument
+    urlQuery = f'{URL}' + query
+    result = requests.get(urlQuery)
+
+    #assigns totalItems and items from the API call to a info object
+    info['totalItems'] = result.json().get('totalItems')
+    info['items'] = result.json().get('items')
+
+    return info
+    
+def parseInfo(info):
+    #returns specific book information
+    book = {}
+    attrs = ['title','authors','description','categories']
+    for attr in attrs: 
+        book[attr] = info.get(attr)
+    return book
+    
+#Creates the command 
+@click.command()
+@click.argument('name')
+def main(name):
+    # Displays all the information related with the query 
+    info = getBooks(name)
+    totalBooks = info.get("totalItems")
+    books = info.get("items")
+    # displays books that contains the query 
+    click.echo(f'TOTAL OF BOOKS FOUND: {totalBooks}')
+    click.echo("BOOKS FOUND: ")
+    # Displays the first 10 books found
+    for count,book in enumerate(books, start=1):
+        info = book.get('volumeInfo')
+        click.echo(f'{count}-{parseInfo(info)}'  )
+
+if __name__== "__main__":
+    main()
